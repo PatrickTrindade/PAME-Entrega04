a2c671ccfe6d5ffe03e98e84cb7640728433bf33
PatrickTrindade <ptk@divulgacao.com.br>
2021-02-03 19:34:02 -0300

    adicionado alguns imports no __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from .config import Config
 from .extensions import db
 
+from .aluno.model import Aluno
+from .boletim.model import Boletim
+from .materia.model import Materia
+from .professor.model import Professor
+from .turma.model import Turma
+
 def create_app():
 
     app = Flask(__name__)
